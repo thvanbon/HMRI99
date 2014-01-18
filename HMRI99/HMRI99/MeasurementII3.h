3839efcee81111448b1b972634b8b07f3d510cc7
Thijs van Bon <tvanb@outlook.com>
2014-01-18 22:02:28 +0000

    Removing "MeasurementII3.h"

--- HMRI99/HMRI99/MeasurementII3.h
+++
@@ -1,9 +0,0 @@
-#import "Measurement.h"
-
-@interface MeasurementII3 : Measurement
-@property float surfaceArea;
-@property float nearFieldCorrection;
-@property float directivityIndex;
-
--(void) calculateSoundPowerLevel;
-@end
